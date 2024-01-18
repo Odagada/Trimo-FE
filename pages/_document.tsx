24c3396d719157dc3b39c9a528315c9b41218ccc
arky02 <kyean07@naver.com>
2024-01-18 15:40:27 +0900

    chore: 프로젝트 기본 세팅, default 세팅 초기회

---
+++ pages/_document.tsx
@@ -0,0 +1,12 @@
+import { Html, Head, Main } from "next/document";
+
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head />
+      <body>
+        <Main />
+      </body>
+    </Html>
+  );
+}
