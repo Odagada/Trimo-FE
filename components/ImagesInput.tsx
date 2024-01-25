b0b0b144fae8a8cbdebed6479176ad8fee643443
rngpwl529 <gooma519@gmail.com>
2024-01-26 00:40:32 +0900

    feat: drag drop 기능 추가

--- components/ImagesInput.tsx
+++ components/ImagesInput.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import { ChangeEvent, useRef, useState } from "react";
+import { ChangeEvent, DragEvent, useState } from "react";
 
 type Image = {
   name: string;
@@ -9,23 +9,46 @@ type Image = {
 export default function ImagesInput() {
   const [isDragging, setIsDragging] = useState(false);
   const [images, setImages] = useState<Image[]>([]);
-  const fileInputRef = useRef(null);
 
-  const onFileSelect = (e: ChangeEvent<HTMLInputElement>) => {
-    const files = e.target.files;
+  const getFiles = (files: FileList | null) => {
     if (files === null) return;
     for (let i = 0; i < files.length; i++) {
       if (!images.some((el) => el.name === files[i].name)) {
-        setImages((prev) => [
-          ...prev,
-          { name: files[i].name, url: URL.createObjectURL(files[i]) },
-        ]);
+        setImages((prev) => [...prev, { name: files[i].name, url: URL.createObjectURL(files[i]) }]);
       }
     }
   };
 
+  const selectFile = (e: ChangeEvent<HTMLInputElement>) => {
+    const files = e.target.files;
+    getFiles(files);
+  };
+
   const deleteFile = (idx: number) => {
     setImages((prev) => prev.filter((_, i) => i !== idx));
+  };
+
+  const handleDragEnter = (e: DragEvent<HTMLElement>) => {
+    e.preventDefault();
+    setIsDragging(true);
+  };
+
+  const handleDragLeave = (e: DragEvent<HTMLElement>) => {
+    e.preventDefault();
+    setIsDragging(false);
+  };
+
+  const handleDragOver = (e: DragEvent<HTMLElement>) => {
+    e.preventDefault();
+    setIsDragging(true);
+    e.dataTransfer.dropEffect = "copy";
+  };
+
+  const handleDrop = (e: DragEvent<HTMLElement>) => {
+    e.preventDefault();
+    setIsDragging(false);
+    const files = e.dataTransfer.files;
+    getFiles(files);
   };
 
   return (
@@ -36,38 +59,32 @@ export default function ImagesInput() {
         style={{ display: "none" }}
         multiple={true}
         accept="image/*"
-        ref={fileInputRef}
-        onChange={onFileSelect}
+        onChange={selectFile}
       />
       <label
         htmlFor="imageUpload"
-        className={`w-full h-20 flex justify-center items-center border-[2px] rounded-lg border-gray-400 border-dashed`}
+        className={`w-full h-20 flex justify-center items-center border-[2px] rounded-lg border-gray-400 border-dashed ${
+          isDragging ? "bg-slate-300" : ""
+        }`}
+        onDragEnter={handleDragEnter}
+        onDragLeave={handleDragLeave}
+        onDragOver={handleDragOver}
+        onDrop={handleDrop}
       >
-        {isDragging ? (
-          <div>파일을 드롭해주세요</div>
-        ) : (
-          <div>클릭 혹은 파일을 드롭해주세요</div>
-        )}
+        <div className="pointer-events-none">
+          {isDragging ? "이곳에 드롭해주세요" : "클릭 혹은 파일을 드롭해주세요"}
+        </div>
       </label>
       <div className="grid w-full grid-cols-5 gap-4 mt-4">
         {images.map((el, idx) => (
-          <div
-            className="relative overflow-hidden rounded-lg aspect-square"
-            key={idx}
-          >
+          <div className="relative overflow-hidden rounded-lg aspect-square" key={idx}>
             <span
               className="h-4 w-4 absolute right-0 z-[2] rounded-full bg-gray-100 flex items-center justify-center cursor-pointer"
               onClick={() => deleteFile(idx)}
             >
               &times;
             </span>
-            <Image
-              src={el.url}
-              alt={el.name}
-              sizes="75px"
-              fill
-              style={{ objectFit: "cover" }}
-            />
+            <Image src={el.url} alt={el.name} sizes="75px" fill style={{ objectFit: "cover" }} />
           </div>
         ))}
       </div>
