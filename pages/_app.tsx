7e914ea97faf7fcf0710c0839f8994b4e6da06ec
arky02 <kyean07@naver.com>
2024-02-02 02:48:38 +0900

    fix: app 파일에 세미콜론 잘못 찍힌 오류 수정

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -17,7 +17,7 @@ export default function App({ Component, pageProps }: AppProps) {
   );
   return (
     <QueryClientProvider client={queryClient}>
-      <Component {...pageProps} />;
+      <Component {...pageProps} />
       <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   );
