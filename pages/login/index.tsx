a22714f49ae0b8808607874e75d7b689c0ce3ccf
arky02 <kyean07@naver.com>
2024-02-06 23:48:30 +0900

    feat(loginpage): 로그인 페이지 초기 세팅

--- pages/login/index.tsx
+++ pages/login/index.tsx
@@ -1,5 +1,19 @@
+import Clickable from "@/components/atoms/Clickable";
+
 function Login() {
-  return <div>LoginPage</div>;
+  return (
+    <div>
+      <Clickable color="white">
+        <button>구글 로그인</button>
+      </Clickable>
+      <Clickable color="kakao" size="medium">
+        <button>카카오 로그인</button>
+      </Clickable>
+      <Clickable color="naver">
+        <button>네이버 로그인</button>
+      </Clickable>
+    </div>
+  );
 }
 
 export default Login;
