4d739d4c3ffa30b5c6df4717dde2cebfceda86e1
rngpwl529 <gooma519@gmail.com>
2024-02-06 12:03:17 +0900

    feat: config 수정

--- next.config.js
+++ next.config.js
@@ -4,12 +4,12 @@ const nextConfig = {
   images: {
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: '**',
+        protocol: "https",
+        hostname: "**",
       },
       {
-        protocol: 'http',
-        hostname: '**',
+        protocol: "http",
+        hostname: "**",
       },
     ],
   },
