dba906de71cda3b36919e36fddb2ddbb4053cf67
ayden94 <144667387+ayden94@users.noreply.github.com>
2024-02-06 23:33:39 +0900

    feat : 태그 기능을 위한 타입 추가

--- types/client.types.ts
+++ types/client.types.ts
@@ -6,3 +6,4 @@ export type TagWeather = "맑음" | "흐림" | "우천" | "눈";
 export type TagMonth = "1월" | "2월" | "3월" | "4월" | "5월" | "6월" | "7월" | "8월" | "9월" | "10월" | "11월" | "12월";
 export type TagType = "맛집" | "관광" | "휴양" | "명소";
 export type TagCompanion = "가족" | "친구" | "연인" | "혼자";
+export type Tag = TagWeather | TagMonth | TagType | TagCompanion;
