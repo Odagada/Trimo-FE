f185355c6f58c01f2cde9d612d60a254cf9da84e
arky02 <kyean07@naver.com>
2024-02-06 23:50:08 +0900

    refactor: signup page button style 추가

--- pages/login/index.tsx
+++ pages/login/index.tsx
@@ -3,13 +3,13 @@ import Clickable from "@/components/atoms/Clickable";
 function Login() {
   return (
     <div>
-      <Clickable color="white">
+      <Clickable color="white" size="medium">
         <button>구글 로그인</button>
       </Clickable>
       <Clickable color="kakao" size="medium">
         <button>카카오 로그인</button>
       </Clickable>
-      <Clickable color="naver">
+      <Clickable color="naver" size="medium">
         <button>네이버 로그인</button>
       </Clickable>
     </div>
