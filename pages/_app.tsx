24ef92f2808a48fe9858e9d0ecbfce299274896f
ayden94 <144667387+ayden94@users.noreply.github.com>
2024-02-06 16:59:30 +0900

    feat : reset css 설정

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,3 +1,4 @@
+import "@/styles/reset.css";
 import "@/styles/globals.css";
 import "@/public/font/font.css";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
@@ -18,7 +19,7 @@ export default function App({ Component, pageProps }: AppProps) {
   );
   return (
     <QueryClientProvider client={queryClient}>
-      <Component {...pageProps} />;
+      <Component {...pageProps} />
       <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   );
