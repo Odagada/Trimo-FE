fec1a0acf74a522b98b20a8ad20d9b8ba617312b
ayden94 <144667387+ayden94@users.noreply.github.com>
2024-02-06 16:03:16 +0900

    feat : 버튼 스타일 레이어 생성

---
+++ components/atoms/Button.tsx
@@ -0,0 +1,37 @@
+import { ButtonColor, ButtonShape, ButtonSize } from "@/types/client.types";
+import { getButtonColorValue, getButtonSizeValue } from "@/utils/getButton";
+import { ReactNode } from "react";
+
+const Button = ({
+  children,
+  className = "",
+  color = "black",
+  shape = "square",
+  size = "large",
+}: {
+  className?: string;
+  disabled?: boolean;
+  children: ReactNode;
+  color?: ButtonColor;
+  shape?: ButtonShape;
+  size?: ButtonSize;
+}) => {
+  const ButtonColor = getButtonColorValue(color);
+  const ButtonSize = getButtonSizeValue(size);
+
+  return (
+    <span
+      className={`${className}
+        ${ButtonColor}
+        ${shape === "square" ? "rounded-10" : "rounded-50"}
+        ${ButtonSize}
+        
+        inline-block text-center
+        w-full`}
+    >
+      {children}
+    </span>
+  );
+};
+
+export default Button;
