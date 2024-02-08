af855bacc3bdddaa8a18c9976aeb58bd641e810a
ayden94 <144667387+ayden94@users.noreply.github.com>
2024-02-08 12:45:00 +0900

    feat : 타입 순서 조정 및 getImoji 함수 생성

---
+++ utils/getImoji.ts
@@ -0,0 +1,58 @@
+import { Tag } from "@/types/client.types";
+
+const getImoji = (tag: Tag) => {
+  let imoji;
+
+  switch (tag) {
+    // 날씨 태그 스위쳐
+    case "맑음":
+      imoji = "☀";
+      break;
+    case "흐림":
+      imoji = "☁️";
+      break;
+
+    case "우천":
+      imoji = "☔";
+      break;
+    case "눈":
+      imoji = "❄️";
+      break;
+
+    // 타입 태그 스위쳐
+    case "맛집":
+      imoji = "🌭";
+      break;
+    case "관광":
+      imoji = "🎡";
+      break;
+    case "휴양":
+      imoji = "🏝";
+      break;
+    case "명소":
+      imoji = "🗻";
+      break;
+
+    // 동행 태그 스위쳐
+    case "가족":
+      imoji = "👨‍👩‍👧";
+      break;
+    case "친구":
+      imoji = "👬";
+      break;
+    case "연인":
+      imoji = "👩‍❤️‍👨";
+      break;
+    case "혼자":
+      imoji = "🕺";
+      break;
+
+    default:
+      imoji = "";
+      break;
+  }
+
+  return tag + imoji;
+};
+
+export default getImoji;
