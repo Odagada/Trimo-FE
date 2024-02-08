3d41d8926a477d86d83126c84183b4bdeef2bfde
arky02 <kyean07@naver.com>
2024-02-08 12:44:23 +0900

    refactor: google map 크기 조정

--- components/molecules/GoogleMapContent.tsx
+++ components/molecules/GoogleMapContent.tsx
@@ -8,7 +8,7 @@ export interface MapContentProps {
 export const GoogleMapContent = ({ locationQuery }: MapContentProps) => {
   const ref = useDrawGoogleMap({ locationQuery });
 
-  return <div ref={ref} className="w-[700px] h-[450px]" />; // 구글 지도 컴포넌트
+  return <div ref={ref} className="w-[792px] h-[430px]" />; // 구글 지도 컴포넌트
 };
 
 export default GoogleMapContent;
