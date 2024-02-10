468d0af85a471ef3e1f6425f1ed1238a6b214487
ayden94 <144667387+ayden94@users.noreply.github.com>
2024-02-10 17:52:36 +0900

    feat : 필요한 형식의 날짜를 반환하는 calcDate 함수 추가

---
+++ utils/calcDate.ts
@@ -0,0 +1,24 @@
+const calcData = (date: string) => {
+  const targetDate = new Date(date);
+
+  const year = targetDate.getFullYear();
+  const month = ("0" + (targetDate.getMonth() + 1)).slice(-2);
+  const day = ("0" + targetDate.getDate()).slice(-2);
+
+  const dateString = year + "." + month + "." + day;
+
+  const hours = ("0" + targetDate.getHours()).slice(-2);
+  const minutes = ("0" + targetDate.getMinutes()).slice(-2);
+
+  const timeString = hours + ":" + minutes;
+
+  const tagMonth = `${String(targetDate.getMonth() + 1)}월`;
+
+  return {
+    dateString,
+    timeString,
+    tagMonth,
+  };
+};
+
+export default calcData;
