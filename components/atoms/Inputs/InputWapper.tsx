8e752f8145560cd95d71b89efe087ee14f13ccfc
arky02 <kyean07@naver.com>
2024-02-15 23:28:28 +0900

    refactor: divOption 받게 적용, 사용자 지정 스타일이 우선 적용되도록 수정

--- components/atoms/Inputs/InputWapper.tsx
+++ components/atoms/Inputs/InputWapper.tsx
@@ -1,14 +1,14 @@
 import { InputWrapperProps } from "@/types/client.types";
 
-function InputWrapper({ title, htmlFor, className = "", errors, children }: InputWrapperProps) {
+function InputWrapper({ title, htmlFor, className = "", errors, children, divOptions }: InputWrapperProps) {
   return (
-    <div className="flex flex-col w-full gap-10">
+    <div className={`flex flex-col w-full gap-10 ${divOptions}`}>
       {title && <p className="text-primary-900 heading5">{title}</p>}
       <label
         htmlFor={htmlFor}
-        className={`${className} ${
+        className={`${
           errors ? "border-error" : "border-gray-300 focus-within:border-primary-600"
-        }  flex w-full flex-row item-center rounded-10 px-16 py-12 bg-white border border-solid tab:px-16 tab:py-14`}
+        }  flex w-full flex-row item-center rounded-10 px-16 py-12 bg-white border border-solid tab:px-16 tab:py-14 ${className} `}
       >
         {children}
       </label>
