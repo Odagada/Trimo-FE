7b1f8b563ff36c5a3a3add5ee09b262502ab3c03
ayden94 <144667387+ayden94@users.noreply.github.com>
2024-02-16 00:01:14 +0900

    fix : api endpoint 수정

--- apis/capsulesQuery.ts
+++ apis/capsulesQuery.ts
@@ -4,13 +4,13 @@ import fetcher from "./axios";
 export const getReview = (reviewId: number) => {
   return {
     queryKey: ["review", reviewId],
-    queryFn: () => fetcher<SingleReviewData>({ method: "get", url: `/users/spots/reviews/${reviewId}` }),
+    queryFn: () => fetcher<SingleReviewData>({ method: "get", url: `/main/spots/reviews/${reviewId}` }),
   };
 };
 
 export const getSpot = (spotId: string) => {
   return {
     queryKey: ["spot", spotId],
-    queryFn: () => fetcher<SpotData>({ method: "get", url: `users/spots/${spotId}` }),
+    queryFn: () => fetcher<SpotData>({ method: "get", url: `/main/spots/${spotId}` }),
   };
 };
