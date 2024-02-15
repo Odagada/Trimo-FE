901b5718b6890562e096f513cbf421a1f5f03282
arky02 <kyean07@naver.com>
2024-02-15 23:32:09 +0900

    refactor: 회원가입 로직 컴포넌트로 분리

--- pages/signup/[provider].tsx
+++ pages/signup/[provider].tsx
@@ -1,74 +1,43 @@
 //http://localhost:3000/signup/google?code=4%2F0AfJohXkZ6kYxGOeqTJMqq3E1QiD_V2Jf1rmTA9RLP9YTOSvJpqrHLcz0aePj7Sm1X7fF7A&scope=email+profile+openid+https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fuserinfo.profile+https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fuserinfo.email&authuser=0&prompt=consenthttp://localhost:3000/signup/google?code=4%2F0AfJohXkZ6kYxGOeqTJMqq3E1QiD_V2Jf1rmTA9RLP9YTOSvJpqrHLcz0aePj7Sm1X7fF7A&scope=email+profile+openid+https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fuserinfo.profile+https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fuserinfo.email&authuser=0&prompt=consent
+import ProgressNavigator from "@/components/molecules/ProgressNavigator";
+import ShadowBox from "@/components/atoms/ShadowBox";
+import Nav from "@/components/molecules/NavigationBar";
 import axios from "axios";
 import { useRouter } from "next/router";
 import { useController, useForm } from "react-hook-form";
 import Select from "react-select";
+import InputWrapper from "@/components/atoms/Inputs/InputWapper";
+import Input from "@/components/atoms/Inputs/Input";
+import Clickable from "@/components/atoms/Clickable";
+import TermsBox from "@/components/molecules/TermsBox";
+import { useEffect, useState } from "react";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import Camera from "@/public/images/camera.png";
+import Image from "next/image";
+import useGetUserSocialInfo from "@/hooks/useGetUserSocialInfo";
+import TermsAgreements from "./components/TermsAgreements";
 
 function SignUp() {
   const router = useRouter();
+
   const { code } = router.query;
   const provider = router.query.provider;
-
   // console.log(code);
   // console.log(router.query.provider);
+  //http://ec2-13-124-115-4.ap-northeast-2.compute.amazonaws.com:8080 - Generated server url
 
-  // axios.post(`http://localhost:8080/login/oauth/${provider}?code=${code}&state=null`);
+  //api/nickname => ok
 
-  const {
-    register,
-    handleSubmit,
-    control,
-    formState: { errors },
-  } = useForm({
-    mode: "onBlur",
-    shouldFocusError: true,
-    reValidateMode: "onChange",
-    defaultValues: { nickName: "", gender: "남성", birthdate: 2000 },
-  });
-
-  const {
-    field: { value: birthdateVal, onChange: onBirthdateChange, ...restField },
-  } = useController({ name: "birthdate", control });
-
-  const birthdateList: { value: number; label: number }[] = [];
-  for (let i = 1960; i < 2015; i++) {
-    birthdateList.push({ value: i, label: i });
-  }
-
-  const onSubmit = (data: { nickName: string; gender: string; birthdate: number }) => {
-    // console.log("data: ", data);
-    // console.log(typeof data);
-  };
+  const userSocialData = useGetUserSocialInfo({ code, provider });
 
   //닉네임/나이/성별
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <input
-        {...register("nickName", {
-          required: true,
-          maxLength: { value: 5, message: "5자 이하의 닉네임을 입력 해 주세요." },
-        })}
-      ></input>
-
-      <Select
-        instanceId="long-value-select"
-        placeholder="생년월일 입력"
-        isClearable
-        options={birthdateList}
-        value={birthdateVal ? birthdateList.find((x) => x.value === birthdateVal) : birthdateVal}
-        onChange={(option) => onBirthdateChange(option)}
-        {...restField}
-      />
-
-      <div>
-        <input type="radio" value="male" {...register("gender")} />
-        남성
-        <input type="radio" value="female" {...register("gender")} />
-        여성
-      </div>
-
-      <button type="submit">submit</button>
-    </form>
+    <div className="h-screen flex w-full flex-col">
+      <Nav navStatus="LoggedOut" />
+      <ShadowBox>
+        <TermsAgreements></TermsAgreements>
+      </ShadowBox>
+    </div>
   );
 }
 
