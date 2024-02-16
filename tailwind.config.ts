2a03b155d35371d322f09aa8b011d693594eac09
ayden94 <144667387+ayden94@users.noreply.github.com>
2024-02-17 01:30:12 +0900

    feat : 태그 자동 횡 스크롤을 위한 애니메이션과 키프레임 추가

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -92,6 +92,22 @@ const config: Config = {
     boxShadow: {
       main: "0px 4px 10px 0px rgba(0, 0, 0, 0.10)",
     },
+    extend: {
+      animation: {
+        marquee: "marquee 50s linear infinite",
+        marquee2: "marquee2 50s linear infinite",
+      },
+      keyframes: {
+        marquee: {
+          "0%": { transform: "translateX(0%)" },
+          "100%": { transform: "translateX(-100%)" },
+        },
+        marquee2: {
+          "0%": { transform: "translateX(100%)" },
+          "100%": { transform: "translateX(0%)" },
+        },
+      },
+    },
   },
   plugins: [require("tailwind-scrollbar-hide")],
 };
