86c2869dd8ffdb6c91eb332c4a29671021fa7e76
arky02 <kyean07@naver.com>
2024-02-16 10:13:32 +0900

    refactor(ShadowBox): width 고정값으로 변경

--- components/atoms/ShadowBox.tsx
+++ components/atoms/ShadowBox.tsx
@@ -6,7 +6,7 @@ interface Props extends Children {
 function ShadowBox({ className = "", children }: Props) {
   return (
     <div
-      className={` ${className} w-fit h-fit px-130 pb-80 pt-10 m-auto flex flex-col justify-center items-center  bg-white rounded-30 shadow-[0_4px_15px_rgb(0,0,0,0.13)] m-50`}
+      className={` ${className} w-800 h-fit px-130 pb-55 pt-10 m-auto flex flex-col justify-center items-center  bg-white rounded-30 shadow-[0_4px_15px_rgb(0,0,0,0.13)] m-50`}
     >
       {children}
     </div>
