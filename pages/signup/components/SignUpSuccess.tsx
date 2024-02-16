ee6165240c9b4fd28c1f0a9a4ab0cf68c9d606ba
arky02 <kyean07@naver.com>
2024-02-16 16:32:59 +0900

    refactor: SignUpSuccess 파일의 문장 constant로 상수화

--- pages/signup/components/SignUpSuccess.tsx
+++ pages/signup/components/SignUpSuccess.tsx
@@ -1,17 +1,14 @@
-import Clickable from "@/components/atoms/Clickable";
-import ShadowBox from "@/components/atoms/ShadowBox";
-import Nav from "@/components/molecules/NavigationBar";
-import ProgressNavigator from "@/components/molecules/ProgressNavigator";
 import Image from "next/image";
 import Camera from "@/public/images/camera.png";
+import { SIGNUP_SUCCESS_MESSAGE, WELCOMING_MESSAGE } from "@/constants/signupConstants";
 
 function SuccessPage({ nickname }: { nickname: string }) {
   return (
     <div className="flex flex-col items-center">
       <Image src={Camera} width={260} height={200} className="mt-46 mb-35" alt="signup success"></Image>
       <div className="flex flex-col gap-15 items-center">
-        <p className="font-bold text-28">{`${nickname}님의 회원가입을 환영합니다.`}</p>
-        <span>지금 바로 다양한 여행 리뷰를 감상해보세요!</span>
+        <p className="font-bold text-28">{`${nickname}${SIGNUP_SUCCESS_MESSAGE}`}</p>
+        <span>{WELCOMING_MESSAGE}</span>
       </div>
     </div>
   );
