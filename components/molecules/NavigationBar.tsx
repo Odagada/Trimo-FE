fd69d2528211ae9dc4890d0d80694b0d891beed6
ayden94 <144667387+ayden94@users.noreply.github.com>
2024-02-17 01:26:42 +0900

    design : nav 바 스타일 변경 및 시맨틱 태그 사용

--- components/molecules/NavigationBar.tsx
+++ components/molecules/NavigationBar.tsx
@@ -2,7 +2,6 @@ import Logo from "@/public/logos/navLogo.png";
 import Image from "next/image";
 import defaultProfile from "@/public/images/defaultProfile.png";
 import Link from "next/link";
-import SearchBar from "../atoms/Inputs/SearchBar";
 import { NavStatus } from "@/types/client.types";
 import useComponentPopup from "@/hooks/useComponentPopup";
 import HeaderDropdown from "../atoms/Dropdowns/HeaderDropdown";
@@ -41,13 +40,13 @@ function Nav({ navStatus = "LoggedIn" }: NavProps) {
   };
 
   return (
-    <div className="h-fit mb-66 flex flex-col w-full">
-      <div className="z-50 h-fit py-21 px-121 fixed top-0 flex flex-wrap items-center justify-between w-full bg-white">
+    <nav className="h-fit mb-66 flex flex-col w-full">
+      <div className="z-50 h-fit py-12 px-121 fixed top-0 flex flex-wrap items-center justify-between w-full bg-white">
         <Image src={Logo} alt="logo" width={78} height={20}></Image>
-        <span id="navSearchBar"></span>
+        <span id="navSearchBar" className="h-50"></span>
         {renderNavbarLeftSide(navStatus)}
       </div>
-    </div>
+    </nav>
   );
 }
 
