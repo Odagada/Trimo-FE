8051e3913b57f4566dd5d62a1e69757c002f2c5e
arky02 <kyean07@naver.com>
2024-02-17 15:55:30 +0900

    refactor: data type 추가

--- hooks/signup/useGetUserSocialInfo.tsx
+++ hooks/signup/useGetUserSocialInfo.tsx
@@ -3,6 +3,7 @@ import axios from "axios";
 import { useRouter } from "next/router";
 import { useMutation } from "@tanstack/react-query";
 import { SIGNUP_SERVER_ERROR } from "@/constants/signupConstants";
+import { UserSocialLoginData } from "@/types/client.types";
 
 interface Props {
   code: string | undefined | string[];
@@ -10,15 +11,15 @@ interface Props {
 }
 function useGetUserSocialInfo({ code, provider }: Props) {
   const router = useRouter();
-  const [data, setData] = useState({ data: { accessToken: null } });
+  const [data, setData] = useState<UserSocialLoginData>();
 
   const { mutate, error } = useMutation({
     mutationFn: () =>
       axios.post(
         `http://ec2-13-124-115-4.ap-northeast-2.compute.amazonaws.com:8080/login/oauth/${provider}?code=${code}&state=null`
       ),
-    onSuccess: (data) => {
-      setData(data);
+    onSuccess: (data: { data: UserSocialLoginData }) => {
+      setData(data.data);
     },
     onError: (error) => {
       alert(`${error.message} ${SIGNUP_SERVER_ERROR} `);
