f9d4830317c2031740c256f90e670400b99fd23f
arky02 <kyean07@naver.com>
2024-02-17 15:57:05 +0900

    feat: signupSuccess page에 빠진 버튼 추가

--- pages/signup/components/SignUpSuccess.tsx
+++ pages/signup/components/SignUpSuccess.tsx
@@ -1,6 +1,8 @@
 import Image from "next/image";
+import Link from "next/link";
 import Camera from "@/public/images/camera.png";
 import { SIGNUP_SUCCESS_MESSAGE, WELCOMING_MESSAGE } from "@/constants/signupConstants";
+import Clickable from "@/components/atoms/Clickable";
 
 function SuccessPage({ nickname }: { nickname: string }) {
   return (
@@ -10,6 +12,11 @@ function SuccessPage({ nickname }: { nickname: string }) {
         <p className="font-bold text-28">{`${nickname}${SIGNUP_SUCCESS_MESSAGE}`}</p>
         <span>{WELCOMING_MESSAGE}</span>
       </div>
+      <Link href={"/"} className="w-full">
+        <Clickable size="large" className="w-full">
+          메인으로 이동
+        </Clickable>
+      </Link>
     </div>
   );
 }
