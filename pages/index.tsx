0ec8d3b715a522440f723dfbba6bbb41ee5d34b3
ayden94 <144667387+ayden94@users.noreply.github.com>
2024-02-18 15:33:47 +0900

    feat : api 연결

--- pages/index.tsx
+++ pages/index.tsx
@@ -16,223 +16,38 @@ import desktopScreenShot from "@/public/images/DesktopScreenShot.png";
 import desktopScreenShot2 from "@/public/images/DesktopScreenShot2.png";
 import Link from "next/link";
 import Image from "next/image";
+import { GetServerSidePropsContext, InferGetServerSidePropsType } from "next";
+import { QueryClient, dehydrate } from "@tanstack/react-query";
+import { getReviewCardArray } from "@/apis/capsulesQuery";
 
-const array: MultiReviewData[] = [
-  {
-    reviewId: 1,
-    title: "제주제주제주빔",
-    tagValues: {
-      weather: "우천",
-    },
-    visitingTime: "1995-12-17T03:24:00",
-    nickName: "본롸",
-    stars: 4,
-  },
-  {
-    reviewId: 1,
-    title: "제주제주제주빔",
-    tagValues: {
-      weather: "우천",
-    },
-    visitingTime: "1995-12-17T03:24:00",
-    nickName: "본롸",
-    stars: 4,
-  },
-  {
-    reviewId: 1,
-    title: "제주제주제주빔",
-    tagValues: {
-      weather: "우천",
-    },
-    visitingTime: "1995-12-17T03:24:00",
-    nickName: "본롸",
-    stars: 4,
-  },
-  {
-    reviewId: 1,
-    title: "제주제주제주빔",
-    tagValues: {
-      weather: "우천",
-    },
-    visitingTime: "1995-12-17T03:24:00",
-    nickName: "본롸",
-    stars: 4,
-  },
-  {
-    reviewId: 1,
-    title: "제주제주제주빔",
-    tagValues: {
-      weather: "우천",
-    },
-    visitingTime: "1995-12-17T03:24:00",
-    nickName: "본롸",
-    stars: 4,
-  },
-  {
-    reviewId: 1,
-    title: "제주제주제주빔",
-    tagValues: {
-      weather: "우천",
-    },
-    visitingTime: "1995-12-17T03:24:00",
-    nickName: "본롸",
-    stars: 4,
-  },
-  {
-    reviewId: 1,
-    title: "제주제주제주빔",
-    tagValues: {
-      weather: "우천",
-    },
-    visitingTime: "1995-12-17T03:24:00",
-    nickName: "본롸",
-    stars: 4,
-  },
-  {
-    reviewId: 1,
-    title: "제주제주제주빔",
-    tagValues: {
-      weather: "우천",
-    },
-    visitingTime: "1995-12-17T03:24:00",
-    nickName: "본롸",
-    stars: 4,
-  },
-  {
-    reviewId: 1,
-    title: "제주제주제주빔",
-    tagValues: {
-      weather: "우천",
-    },
-    visitingTime: "1995-12-17T03:24:00",
-    nickName: "본롸",
-    stars: 4,
-  },
-  {
-    reviewId: 1,
-    title: "제주제주제주빔",
-    tagValues: {
-      weather: "우천",
-    },
-    visitingTime: "1995-12-17T03:24:00",
-    nickName: "본롸",
-    stars: 4,
-  },
-  {
-    reviewId: 1,
-    title: "제주제주제주빔",
-    tagValues: {
-      weather: "우천",
-    },
-    visitingTime: "1995-12-17T03:24:00",
-    nickName: "본롸",
-    stars: 4,
-  },
-  {
-    reviewId: 1,
-    title: "제주제주제주빔",
-    tagValues: {
-      weather: "우천",
-    },
-    visitingTime: "1995-12-17T03:24:00",
-    nickName: "본롸",
-    stars: 4,
-  },
-  {
-    reviewId: 1,
-    title: "제주제주제주빔",
-    tagValues: {
-      weather: "우천",
-    },
-    visitingTime: "1995-12-17T03:24:00",
-    nickName: "본롸",
-    stars: 4,
-  },
-  {
-    reviewId: 1,
-    title: "제주제주제주빔",
-    tagValues: {
-      weather: "우천",
-    },
-    visitingTime: "1995-12-17T03:24:00",
-    nickName: "본롸",
-    stars: 4,
-  },
-  {
-    reviewId: 1,
-    title: "제주제주제주빔",
-    tagValues: {
-      weather: "우천",
-    },
-    visitingTime: "1995-12-17T03:24:00",
-    nickName: "본롸",
-    stars: 4,
-  },
-  {
-    reviewId: 1,
-    title: "제주제주제주빔",
-    tagValues: {
-      weather: "우천",
-    },
-    visitingTime: "1995-12-17T03:24:00",
-    nickName: "본롸",
-    stars: 4,
-  },
-  {
-    reviewId: 1,
-    title: "제주제주제주빔",
-    tagValues: {
-      weather: "우천",
-    },
-    visitingTime: "1995-12-17T03:24:00",
-    nickName: "본롸",
-    stars: 4,
-  },
-  {
-    reviewId: 1,
-    title: "제주제주제주빔",
-    tagValues: {
-      weather: "우천",
-    },
-    visitingTime: "1995-12-17T03:24:00",
-    nickName: "본롸",
-    stars: 4,
-  },
-  {
-    reviewId: 1,
-    title: "제주제주제주빔",
-    tagValues: {
-      weather: "우천",
-    },
-    visitingTime: "1995-12-17T03:24:00",
-    nickName: "본롸",
-    stars: 4,
-  },
-  {
-    reviewId: 1,
-    title: "제주제주제주빔",
-    tagValues: {
-      weather: "우천",
-    },
-    visitingTime: "1995-12-17T03:24:00",
-    nickName: "본롸",
-    stars: 4,
-  },
-];
+export const getServerSideProps = async (context: GetServerSidePropsContext) => {
+  try {
+    // const accessToken = getAccessTokenFromCookie(context) as string;\
 
-const tagClassArray = [
-  "top-0 py-12 animate-marquee whitespace-nowrap",
-  "absolute top-0 py-12 animate-marquee2 whitespace-nowrap",
-];
+    const queryClient = new QueryClient();
 
-export default function Landing() {
+    const { data: popularReviewCardArray } = await queryClient.fetchQuery(getReviewCardArray("populer"));
+    const { data: recentReviewCardArray } = await queryClient.fetchQuery(getReviewCardArray("recent"));
+
+    return {
+      props: { popularReviewCardArray, recentReviewCardArray, dehydratedState: dehydrate(queryClient) },
+    };
+  } catch {
+    return { notFound: true };
+  }
+};
+
+export default function Landing({
+  popularReviewCardArray,
+  recentReviewCardArray,
+}: InferGetServerSidePropsType<typeof getServerSideProps>) {
   return (
     <>
       <Nav />
 
       <HeroSection />
 
-      <CardSection />
+      <CardSection recent={recentReviewCardArray} populer={popularReviewCardArray} />
 
       <TagSection />
 
@@ -265,7 +80,7 @@ const HeroSection = () => {
   );
 };
 
-const CardSection = () => {
+const CardSection = ({ recent, populer }: { recent: MultiReviewData[]; populer: MultiReviewData[] }) => {
   return (
     <>
       <section className="flex flex-col items-center text-center pt-48 pb-72 gap-8 bg-gray-10">
@@ -279,8 +94,8 @@ const CardSection = () => {
 
       <section className="pb-129 bg-gray-10">
         <div className="flex flex-col gap-41">
-          <MultiReviewCardSlider title="최신리뷰" align="left" reviewCards={array} />
-          <MultiReviewCardSlider title="인기리뷰" align="right" reviewCards={array} />
+          <MultiReviewCardSlider title="최신리뷰" align="left" reviewCards={recent} />
+          <MultiReviewCardSlider title="인기리뷰" align="right" reviewCards={populer} />
         </div>
       </section>
     </>
@@ -301,6 +116,11 @@ const TagSection = () => {
     "친구",
     "연인",
     "혼자",
+  ];
+
+  const tagClassArray = [
+    "top-0 py-12 animate-marquee whitespace-nowrap",
+    "absolute top-0 py-12 animate-marquee2 whitespace-nowrap",
   ];
 
   return (
@@ -335,43 +155,6 @@ const TagSection = () => {
             </div>
           );
         })}
-      </div>
-    </section>
-  );
-};
-
-const DeviceSection = () => {
-  return (
-    <section className="bg-gray-10 pb-52">
-      <div className="w-1038 flex flex-col gap-12 mx-auto">
-        <h3 className={`bg-white inline px-15 py-8 rounded-100 shadow-main w-fit text-18 font-bold leading-27`}>
-          디바이스 지원
-        </h3>
-
-        <div className="gap-24 flex">
-          <div className="rounded-30 bg-gray-60 w-330 h-430 p-24 overflow-hidden">
-            <p className="text-right leading-18 text-12 text-gray-30">Mobile</p>
-            <Image className="mx-auto mt-33" src={mobile} alt="" width={237} />
-          </div>
-
-          <div className="rounded-30 bg-gray-60 w-330 h-430 p-24 overflow-hidden flex flex-col justify-between">
-            <p className="text-right text-white font-bold text-24 leading-36">
-              언제 어디서나
-              <br />
-              <span className="text-primary">간편하게</span>
-              <br />
-              작성할 수 있어요
-            </p>
-            <div className="rounded-100 flex items-center justify-center bg-primary w-120 h-120">
-              <Image src={quill} alt="" width={47} height={45} />
-            </div>
-          </div>
-
-          <div className="rounded-30 bg-gray-60 w-330 h-430 p-24 overflow-hidden">
-            <p className="text-right leading-18 text-12 text-gray-30">Desktop</p>
-            <Image className="ml-61 mt-85" src={desktop} alt="" width={270} />
-          </div>
-        </div>
       </div>
     </section>
   );
@@ -422,7 +205,44 @@ const ServiceExplainSection = () => {
         </div>
 
         <div className="w-1/2 h-652 overflow-hidden relative">
-          <Image className="absolute left-0 min-w-882" src={desktopScreenShot2} alt="" width={882} />
+          <Image className="absolute left-0 min-w-882" src={desktopScreenShot} alt="" width={882} />
+        </div>
+      </div>
+    </section>
+  );
+};
+
+const DeviceSection = () => {
+  return (
+    <section className="bg-gray-10 pb-52">
+      <div className="w-1038 flex flex-col gap-12 mx-auto">
+        <h3 className={`bg-white inline px-15 py-8 rounded-100 shadow-main w-fit text-18 font-bold leading-27`}>
+          디바이스 지원
+        </h3>
+
+        <div className="gap-24 flex">
+          <div className="rounded-30 bg-gray-60 w-330 h-430 p-24 overflow-hidden">
+            <p className="text-right leading-18 text-12 text-gray-30">Mobile</p>
+            <Image className="mx-auto mt-33" src={mobile} alt="" width={237} />
+          </div>
+
+          <div className="rounded-30 bg-gray-60 w-330 h-430 p-24 overflow-hidden flex flex-col justify-between">
+            <p className="text-right text-white font-bold text-24 leading-36">
+              언제 어디서나
+              <br />
+              <span className="text-primary">간편하게</span>
+              <br />
+              작성할 수 있어요
+            </p>
+            <div className="rounded-100 flex items-center justify-center bg-primary w-120 h-120">
+              <Image src={quill} alt="" width={47} height={45} />
+            </div>
+          </div>
+
+          <div className="rounded-30 bg-gray-60 w-330 h-430 p-24 overflow-hidden">
+            <p className="text-right leading-18 text-12 text-gray-30">Desktop</p>
+            <Image className="ml-61 mt-85" src={desktop} alt="" width={270} />
+          </div>
         </div>
       </div>
     </section>
