ff859d025fe4e825b70231ce95772fbba2b0a370
ayden94 <144667387+ayden94@users.noreply.github.com>
2024-02-20 21:49:48 +0900

    style : 컬러 잘못 지정된 부분 수정

--- pages/404/index.tsx
+++ pages/404/index.tsx
@@ -12,7 +12,7 @@ function NotFoundPage() {
         <p className="text-36 font-bold leading-54 text-gray-30 mb-64">Not Found</p>
 
         <h3 className="text-28 font-bold leading-42 mb-8">죄송합니다. 페이지를 찾을 수 없습니다.</h3>
-        <p className="text-16 leading-24 mb-28">
+        <p className="text-16 leading-24 mb-28 text-gray-50">
           페이지의 주소가 잘못 입력 되었거나,
           <br />
           주소가 변경 혹은 삭제되어 요청하신 페이지를 찾을 수 없습니다.
