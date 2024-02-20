797208a36e5e86ba28e8de8ebd8a1f25aed2536e
arky02 <kyean07@naver.com>
2024-02-21 01:59:57 +0900

    feat(Modal)

---
+++ components/molecules/Modal.tsx
@@ -0,0 +1,37 @@
+import { useEffect } from "react";
+import ReactDOM from "react-dom";
+
+interface Props {
+  title: string;
+  description: string;
+  buttonText?: string;
+  handleOKClick: () => void;
+  handleModalClose: () => void;
+}
+
+function Modal({ title, description, buttonText = "확인", handleOKClick, handleModalClose }: Props) {
+  const portalDiv = document.querySelector("#modal");
+  if (!portalDiv) return null;
+
+  const onOKClick = () => {
+    handleOKClick();
+    handleModalClose();
+  };
+
+  return ReactDOM.createPortal(
+    <div>
+      <div className="fixed w-full h-full bg-[rgba(0,0,0,0.2)] z-10 left-0 top-0" onClick={handleModalClose}></div>
+      <div className="bg-white z-100 w-440 h-220 fixed pb-58 flex flex-col justify-center items-center -translate-x-1/2 -translate-y-1/2 z-[100] shadow-[0_2pc_12px_0px_rgba(0,0,0,0.08)] gap-5 rounded-[15px] left-1/2 top-1/2">
+        <span className="text-zinc-800 text-lg font-bold ">{title}</span>
+        {description}
+        <button className="w-full h-60 fixed top-162 bg-black text-white rounded-b-[15px]" onClick={onOKClick}>
+          {buttonText}
+        </button>
+      </div>
+    </div>,
+    portalDiv
+  );
+}
+
+export default Modal;
+// min-h-[240px]
