5962e60675288c43f9c19dc2dad912f5e223def7
arky02 <kyean07@naver.com>
2024-02-22 03:23:01 +0900

    refactor: 삽질 끝에 드디어 Modal.stories 작동되게 구현

--- stories/assets/Modal.stories.tsx
+++ stories/assets/Modal.stories.tsx
@@ -1,9 +1,9 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import { Meta, StoryFn } from "@storybook/react";
 import Modal from "@/components/molecules/Modal";
 
 export default {
-  title: "Components/Modal",
+  title: "element/Modal",
   component: Modal,
 } as Meta<typeof Modal>;
 
@@ -11,26 +11,17 @@ const Template: StoryFn<typeof Modal> = (args) => {
   const [isOpen, setIsOpen] = useState(false);
 
   return (
-    <>
-      <button onClick={() => setIsOpen(true)}>Click to open modal</button>
-      <Modal
-        isOpen={isOpen}
-        title="모달 제목"
-        description="모달 설명"
-        buttonText="확인"
-        handleOKClick={() => {}}
-        handleModalClose={() => setIsOpen(false)}
-      />
-    </>
+    <div>
+      <button onClick={() => setIsOpen(true)}>Open Modal!</button>
+      <Modal {...args} isOpen={isOpen} onClose={() => setIsOpen(false)} />
+    </div>
   );
 };
 
-export const ModalComponent = Template.bind({});
-
-ModalComponent.args = {
-  title: "모달 제목dd",
-  description: "모달 설명dd",
+export const ModalStory = Template.bind({});
+ModalStory.args = {
+  isOpen: false,
+  title: "모달 제목",
+  description: "모달 설명입니다!!",
   buttonText: "확인",
-  handleModalClose: () => {},
-  handleOKClick: () => {},
 };
