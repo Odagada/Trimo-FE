4995f15b3328e31e63d6f8abc598a80b508bc639
arky02 <kyean07@naver.com>
2024-02-22 17:42:18 +0900

    feat(validateByLoginStatus)

---
+++ utils/validateByLoginStatus.ts
@@ -0,0 +1,21 @@
+import { redirect } from "next/navigation";
+import { useRouter } from "next/router";
+import { useCallback } from "react";
+
+interface Props {
+  statusToBlock: "Login" | "Logout";
+  redirectUri: string;
+  accessToken: string | null;
+}
+
+export const redirectByLoginStatus = ({ statusToBlock, redirectUri, accessToken }: Props) => {
+  if ((statusToBlock === "Login" && accessToken) || (statusToBlock === "Logout" && !accessToken)) {
+    redirect(redirectUri);
+    // alert(statusToBlock === "Login" ? "이미 로그인 된 상태입니다." : "로그인 후 이용해주세요");
+  }
+  return;
+};
+
+export const isLoggedIn = (accessToken: string | null) => {
+  return accessToken ? true : false;
+};
