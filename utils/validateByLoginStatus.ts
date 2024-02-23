20f369c1d10ffe05424f6035855e2f819986950b
arky02 <kyean07@naver.com>
2024-02-23 17:43:47 +0900

    fix: serverside에서 toast 띄워서 생기는 오류 방지

--- utils/validateByLoginStatus.ts
+++ utils/validateByLoginStatus.ts
@@ -7,7 +7,7 @@ interface Props {
 
 export const validateRedirectionByLoginStatus = ({ statusToBlock, accessToken }: Props) => {
   if ((statusToBlock === "Login" && accessToken) || (statusToBlock === "Logout" && !accessToken)) {
-    makeToast(statusToBlock === "Login" ? "이미 로그인 된 상태입니다." : "로그인 후 이용해주세요", "error");
+    // makeToast(statusToBlock === "Login" ? "이미 로그인 된 상태입니다." : "로그인 후 이용해주세요", "error");
     return true;
   }
   return false;
