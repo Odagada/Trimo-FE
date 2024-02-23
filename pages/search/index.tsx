c8ffde1d41bb5cda9b6a5dd66abf9d4e2a2f66ae
rngpwl529 <gooma519@gmail.com>
2024-02-23 14:04:16 +0900

    feat: nav 로그인 상태 추가

--- pages/search/index.tsx
+++ pages/search/index.tsx
@@ -1,15 +1,29 @@
-import OrderDropdown from "@/components/atoms/Dropdowns/OrderDropdown";
 import Footer from "@/components/atoms/Footer";
 import SearchBar from "@/components/atoms/Inputs/SearchBar";
 import FilterDropdown from "@/components/molecules/FilterDropdown";
 import Nav from "@/components/molecules/NavigationBar";
 import SearchContent from "@/components/organisms/SearchContent";
+import { getAccessTokenFromCookie } from "@/utils/getAccessTokenFormCookie";
+import { isLoggedIn } from "@/utils/validateByLoginStatus";
+import { GetServerSidePropsContext, InferGetServerSidePropsType } from "next";
 
-export default function Search() {
+export const getServerSideProps = async (context: GetServerSidePropsContext) => {
+  try {
+    const accessToken = await getAccessTokenFromCookie(context);
+
+    return {
+      props: { isLoggedIn: isLoggedIn(accessToken) },
+    };
+  } catch {
+    return { notFound: true };
+  }
+};
+
+export default function Search({ isLoggedIn }: InferGetServerSidePropsType<typeof getServerSideProps>) {
   return (
     <div className="flex flex-col justify-between min-h-screen">
       <div>
-        <Nav />
+        <Nav isLoggedIn={isLoggedIn} />
         <div className="pt-23 pb-15">
           <SearchBar size="small" />
         </div>
