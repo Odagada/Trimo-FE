83195ff50db66512dac5f5be1e40c1e1069bc319
rngpwl529 <gooma519@gmail.com>
2024-02-23 12:19:33 +0900

    feat: month 포맷 수정

--- components/molecules/ReviewList.tsx
+++ components/molecules/ReviewList.tsx
@@ -4,7 +4,7 @@ import ReviewCard from "@/components/molecules/ReviewCard";
 
 export default function ReviewList({ data }: { data: MultiReviewData[] }) {
   return (
-    <div className="gap-x-24 gap-y-44 laptop:grid-cols-4 tablet:grid-cols-3 mobile:grid-cols-2 mt-13 grid w-full">
+    <div className="gap-x-24 gap-y-44 mt-13 flex flex-wrap w-full">
       {data?.map((el, idx) => (
         <ReviewCard key={idx} review={el} />
       ))}
