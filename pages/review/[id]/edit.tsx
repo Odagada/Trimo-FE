8debb13cc95c01151920aac9dc9fca4387f9557c
Accept77 <accept_agreed@kakao.com>
2024-02-23 11:05:09 +0900

    feat: [id] 폴더 구조 변경

---
+++ pages/review/[id]/edit.tsx
@@ -0,0 +1,13 @@
+import Footer from "@/components/atoms/Footer";
+import Nav from "@/components/molecules/NavigationBar";
+import PlaceForm from "@/components/organisms/PlaceForm";
+
+export default function reviewEdit() {
+  return (
+    <>
+      <Nav />
+      <PlaceForm />
+      <Footer />
+    </>
+  );
+}
