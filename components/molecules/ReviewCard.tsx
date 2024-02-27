0c34e9ea63916176c82f3e2fe1f7617671331f35
arky02 <kyean07@naver.com>
2024-02-27 20:27:20 +0900

    merge develop

--- components/molecules/ReviewCard.tsx
+++ components/molecules/ReviewCard.tsx
@@ -16,7 +16,7 @@ export default function ReviewCard({ review }: Props) {
   return (
     <Link
       href={`/review/${reviewId}`}
-      className="shadow-main rounded-10 w-282 h-330 flex flex-col items-center p-8 bg-white select-none"
+      className="shadow-main rounded-10 tablet:w-282 tablet:h-330 w-155 h-205 tablet:p-8 flex flex-col items-center p-4 bg-white select-none"
     >
       <div className="h-240 relative w-full overflow-hidden">
         <Image
@@ -30,19 +30,23 @@ export default function ReviewCard({ review }: Props) {
           className="rounded-10"
         />
       </div>
-      <div className="flex flex-col w-full gap-8 px-16 pb-12 mt-8">
-        <div className="flex items-center justify-between">
-          <h3 className="heading6 w-190 text-ellipsis whitespace-nowrap overflow-hidden">{title}</h3>
-          <span className="small-text w-54 text-ellipsis whitespace-nowrap overflow-hidden">by{nickName}</span>
+      <div className="grid w-full grid-cols-[auto_3.7rem] grid-rows-[2rem_2rem_2rem] p-4 pb-6 tablet:grid-rows-[2.8rem_2.6rem] tablet:grid-cols-[auto_4.8rem] tablet:gap-x-14 tablet:gap-y-8 items-center">
+        <h3 className="text-14 leading-20 text-ellipsis whitespace-nowrap tablet:heading6 tablet:col-span-1 col-span-2 overflow-hidden font-bold">
+          {title}
+        </h3>
+        <div className="text-10 leading-20 text-ellipsis whitespace-nowrap tablet:small-text overflow-hidden">
+          <span>by {nickName}</span>
         </div>
-        <div className="small-text flex justify-between">
-          <div className="flex gap-4">
-            <div className="bg-gray-10 w-43 px-2 py-4 text-center rounded-full">{tagMonth}</div>
-            {tagValues &&
-              Object.entries(tagValues).map(([key, value]: [string, string]) => (
-                <ReviewCardTag value={value} key={key + reviewId} />
-              ))}
+        <div className="tablet:col-span-1 flex col-span-2 gap-4">
+          <div className="text-10 bg-gray-10 flex-center tablet:small-text tablet:w-43 tablet:py-4 w-32 rounded-full">
+            {tagMonth}
           </div>
+          {tagValues &&
+            Object.entries(tagValues).map(([key, value]: [string, string]) => (
+              <ReviewCardTag value={value} key={key + reviewId} />
+            ))}
+        </div>
+        <div className="flex justify-end col-start-2 row-start-2">
           <SingleStarRate rate={stars} />
         </div>
       </div>
