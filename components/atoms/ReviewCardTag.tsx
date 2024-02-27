f93ea1d808cd4f5348ecfc2e469e64c8d43de278
rngpwl529 <gooma519@gmail.com>
2024-02-27 12:03:09 +0900

    refact: 반응형 기준 tablet으로 변경

--- components/atoms/ReviewCardTag.tsx
+++ components/atoms/ReviewCardTag.tsx
@@ -7,7 +7,7 @@ export default function ReviewCardTag({ value }: Props) {
   return (
     <>
       {value !== "NONE" && (
-        <div className="text-10 bg-gray-10 flex-center mobile:small-text mobile:w-43 mobile-py-4 w-32 rounded-full">
+        <div className="text-10 bg-gray-10 flex-center tablet:small-text tablet:w-43 tablet-py-4 w-32 rounded-full">
           {value}
         </div>
       )}
--- components/molecules/ReviewCard.tsx
+++ components/molecules/ReviewCard.tsx
@@ -16,7 +16,7 @@ export default function ReviewCard({ review }: Props) {
   return (
     <Link
       href={`/review/${reviewId}`}
-      className="shadow-main rounded-10 mobile:w-282 mobile:h-330 w-155 h-205 mobile:p-8 flex flex-col items-center p-4 bg-white select-none"
+      className="shadow-main rounded-10 tablet:w-282 tablet:h-330 w-155 h-205 tablet:p-8 flex flex-col items-center p-4 bg-white select-none"
     >
       <div className="h-240 relative w-full overflow-hidden">
         <Image
@@ -30,15 +30,15 @@ export default function ReviewCard({ review }: Props) {
           className="rounded-10"
         />
       </div>
-      <div className="grid w-full grid-cols-[auto_3.7rem] grid-rows-[2rem_2rem_2rem] p-4 pb-6 mobile:grid-rows-[2.8rem_2.6rem] mobile:grid-cols-[auto_4.8rem] mobile:gap-x-14 mobile:gap-y-8 items-center">
-        <h3 className="text-14 leading-20 text-ellipsis whitespace-nowrap mobile:heading6 mobile:col-span-1 col-span-2 overflow-hidden font-bold">
+      <div className="grid w-full grid-cols-[auto_3.7rem] grid-rows-[2rem_2rem_2rem] p-4 pb-6 tablet:grid-rows-[2.8rem_2.6rem] tablet:grid-cols-[auto_4.8rem] tablet:gap-x-14 tablet:gap-y-8 items-center">
+        <h3 className="text-14 leading-20 text-ellipsis whitespace-nowrap tablet:heading6 tablet:col-span-1 col-span-2 overflow-hidden font-bold">
           {title}
         </h3>
-        <div className="text-10 leading-20 text-ellipsis whitespace-nowrap mobile:small-text overflow-hidden">
+        <div className="text-10 leading-20 text-ellipsis whitespace-nowrap tablet:small-text overflow-hidden">
           <span>by {nickName}</span>
         </div>
-        <div className="mobile:col-span-1 flex col-span-2 gap-4">
-          <div className="text-10 bg-gray-10 flex-center mobile:small-text mobile:w-43 mobile:py-4 w-32 rounded-full">
+        <div className="tablet:col-span-1 flex col-span-2 gap-4">
+          <div className="text-10 bg-gray-10 flex-center tablet:small-text tablet:w-43 tablet:py-4 w-32 rounded-full">
             {tagMonth}
           </div>
           {tagValues &&
