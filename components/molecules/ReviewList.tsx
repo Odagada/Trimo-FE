04fdfb73b8c3b1fef99859ff93b023e8c3e874f6
Accept77 <accept_agreed@kakao.com>
2024-02-29 04:49:24 +0900

    fix: lint

--- components/molecules/ReviewList.tsx
+++ components/molecules/ReviewList.tsx
@@ -5,7 +5,9 @@ import ReviewCard from "@/components/molecules/ReviewCard";
 export default function ReviewList({ data }: { data: MultiReviewData[] }) {
   return (
     <div className="mt-13 grid grid-cols-2 gap-x-24 gap-y-44 laptop:grid-cols-3 desktop:grid-cols-4">
-      {data?.map((el, idx) => <ReviewCard key={idx} review={el} />)}
+      {data?.map((el, idx) => (
+        <ReviewCard key={idx} review={el} />
+      ))}
     </div>
   );
 }
