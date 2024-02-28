aa478945f500f869d132af899b91cda122c962e9
Accept77 <accept_agreed@kakao.com>
2024-02-28 19:15:07 +0900

    fix: review page 주석 해제

--- pages/review/index.tsx
+++ pages/review/index.tsx
@@ -5,31 +5,31 @@ import { GetServerSidePropsContext, InferGetServerSidePropsType } from "next";
 import { getAccessTokenFromCookie } from "@/utils/getAccessTokenFormCookie";
 import { isLoggedIn, validateRedirectionByLoginStatus } from "@/utils/validateByLoginStatus";
 
-// export const getServerSideProps = async (context: GetServerSidePropsContext) => {
-//   try {
-//     const accessToken = await getAccessTokenFromCookie(context);
+export const getServerSideProps = async (context: GetServerSidePropsContext) => {
+  try {
+    const accessToken = await getAccessTokenFromCookie(context);
 
-//     const isRedirectNeeded = validateRedirectionByLoginStatus({
-//       statusToBlock: "Logout",
-//       accessToken,
-//     });
+    const isRedirectNeeded = validateRedirectionByLoginStatus({
+      statusToBlock: "Logout",
+      accessToken,
+    });
 
-//     if (isRedirectNeeded) {
-//       return {
-//         redirect: {
-//           destination: "/login",
-//           permanent: false,
-//         },
-//       };
-//     } else {
-//       return {
-//         props: { isLoggedIn: isLoggedIn(accessToken) },
-//       };
-//     }
-//   } catch {
-//     return { notFound: true };
-//   }
-// };
+    if (isRedirectNeeded) {
+      return {
+        redirect: {
+          destination: "/login",
+          permanent: false,
+        },
+      };
+    } else {
+      return {
+        props: { isLoggedIn: isLoggedIn(accessToken) },
+      };
+    }
+  } catch {
+    return { notFound: true };
+  }
+};
 
 export default function ReviewWrite({ isLoggedIn }: InferGetServerSidePropsType<typeof getServerSideProps>) {
   return (
