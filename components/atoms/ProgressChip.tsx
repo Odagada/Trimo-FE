1159526114dcb1fc849ed353df1fb798acfd3bb0
arky02 <kyean07@naver.com>
2024-02-28 18:19:29 +0900

    feat: ProgressChip 모바일 반응형 추가

--- components/atoms/ProgressChip.tsx
+++ components/atoms/ProgressChip.tsx
@@ -4,9 +4,11 @@ interface ChipProps {
 }
 function ProgressChip({ num, isOnProgress = false }: ChipProps) {
   return isOnProgress ? (
-    <div className="w-[30px] h-[30px]  bg-black rounded-full text-center text-white py-3 text-16 text-bold">{num}</div>
+    <div className="flex justify-center items-center tablet:w-30 tablet:h-30 w-17 h-17 bg-black rounded-full text-white tablet:text-16 text-10 font-bold">
+      {num}
+    </div>
   ) : (
-    <div className="w-[30px] h-[30px]  bg-white rounded-full border border-zinc-800 text-center py-2 text-16 text-bold">
+    <div className="flex justify-center items-center tablet:w-30 tablet:h-30  w-17 h-17 border-[0.6px] bg-white rounded-full tablet:border border-zinc-800 tablet:text-16 text-10 font-bold">
       {num}
     </div>
   );
