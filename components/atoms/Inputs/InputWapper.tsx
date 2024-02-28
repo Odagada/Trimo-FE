0424553111398c9373cc913447f7fa3a0837e5cd
arky02 <kyean07@naver.com>
2024-02-28 18:18:48 +0900

    feat: Input Wrapper 모바일 반응형 적용

--- components/atoms/Inputs/InputWapper.tsx
+++ components/atoms/Inputs/InputWapper.tsx
@@ -2,13 +2,13 @@ import { InputWrapperProps } from "@/types/client.types";
 
 function InputWrapper({ title, htmlFor, className = "", errors, children, divOptions }: InputWrapperProps) {
   return (
-    <div className={`flex flex-col w-full gap-10 relative ${divOptions}`}>
-      {title && <p className="text-primary-900 heading5">{title}</p>}
+    <div className={`flex flex-col w-full gap-10 maxTablet:gap-4 relative ${divOptions}`}>
+      {title && <p className="text-primary-900 heading5 maxTablet:text-14">{title}</p>}
       <label
         htmlFor={htmlFor}
         className={`${
           errors ? "border-error" : "border-gray-300 focus-within:border-primary-600"
-        }  flex w-full flex-row item-center rounded-10 px-16 py-12 bg-white border border-solid tab:px-16 tab:py-14 ${className} `}
+        }  flex w-full flex-row item-center rounded-10 px-16 py-12 bg-white border border-solid tab:px-16 tab:py-14maxTablet:text-14 ${className} `}
       >
         {children}
       </label>
