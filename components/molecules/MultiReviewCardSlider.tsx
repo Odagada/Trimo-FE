4e4f964b3229ef2fad1d2b4a3eaec0d87d68bb19
ayden94 <144667387+ayden94@users.noreply.github.com>
2024-02-29 03:55:45 +0900

    feat : format

--- components/molecules/MultiReviewCardSlider.tsx
+++ components/molecules/MultiReviewCardSlider.tsx
@@ -1,7 +1,7 @@
 import { MultiReviewData } from "@/types/server.types";
 import ReviewCard from "./ReviewCard";
 import { WheelEvent, useRef } from "react";
-import ScrollContainer from 'react-indiana-drag-scroll'
+import ScrollContainer from "react-indiana-drag-scroll";
 
 const MultiReviewCardSlider = ({
   title,
@@ -23,28 +23,22 @@ const MultiReviewCardSlider = ({
   // };
 
   return (
-      <div className={`flex flex-col gap-12 ${align === "left" ? "items-start" : "items-end"}`}>
+    <div className={`flex flex-col gap-12 ${align === "left" ? "items-start" : "items-end"}`}>
+      <h3
+        className={`${
+          align === "left" ? "ml-64 laptop:ml-260 tablet:ml-122" : "mr-64 laptop:mr-260 tablet:mr-122"
+        } leading-16 inline w-fit rounded-100 bg-white px-12 py-6 text-11 font-bold shadow-main laptop:px-15 laptop:py-8 laptop:text-14 laptop:leading-21`}
+      >
+        {title}
+      </h3>
 
-
-
-        <h3
-          className={`${
-            align === "left" ? "ml-64 laptop:ml-260 tablet:ml-122" : "mr-64 laptop:mr-260 tablet:mr-122"
-          } leading-16 inline w-fit rounded-100 bg-white px-12 py-6 text-11 font-bold shadow-main laptop:px-15 laptop:py-8 laptop:text-14 laptop:leading-21`}
-        >
-          {title}
-        </h3>
-
-
-
-
-        <div
-          className={`${
-            align === "left" ? "flex-row" : "flex-row-reverse"
-          } flex w-full gap-9 pb-10 scrollbar-hide laptop:gap-24 tablet:gap-16`}
-        >
-
-<ScrollContainer vertical={true}     
+      <div
+        className={`${
+          align === "left" ? "flex-row" : "flex-row-reverse"
+        } flex w-full gap-9 pb-10 scrollbar-hide laptop:gap-24 tablet:gap-16`}
+      >
+        <ScrollContainer
+          vertical={true}
           className={`scroll-container ${
             align === "left" ? "flex-row" : "flex-row-reverse"
           } flex w-full gap-9 pb-10 scrollbar-hide laptop:gap-24 tablet:gap-16scroll-container`}
@@ -54,16 +48,9 @@ const MultiReviewCardSlider = ({
             return <ReviewCard key={i} review={review} />;
           })}
           <div className="min-w-55 laptop:min-w-236 tablet:min-w-105"></div>
-
-
-          </ScrollContainer>
-        </div>
-
-
-
-
-
+        </ScrollContainer>
       </div>
+    </div>
   );
 };
 
