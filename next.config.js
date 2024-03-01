a257775c4f0b4100a5df1f373d4375c833bbe185
ayden94 <144667387+ayden94@users.noreply.github.com>
2024-03-01 23:35:02 +0900

    fix : getUser에 enabled 추가

--- next.config.js
+++ next.config.js
@@ -2,7 +2,12 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
+    formats: ["image/avif", "image/webp"],
     remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "odagada.s3.ap-northeast-2.amazonaws.com/",
+      },
       {
         protocol: "https",
         hostname: "**",
